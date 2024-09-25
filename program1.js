d27596479b240a91cb691e7c14af97c6e900e9b3
amitkumarisdevelopeer <130381253+amitkumarisdevelopeer@users.noreply.github.com>
2024-09-25 15:51:10 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    return /^[a-zA-Z0-9]+$/.test(s);
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (mapping[char]) { // If it's a closing bracket
+            if (stack.pop() !== mapping[char]) {
+                return false; // Mismatched closing bracket
+            }
+        } else {
+            stack.push(char); // Push opening bracket to stack
+        }
+    }
+
+    return stack.length === 0; // True if no unmatched opening brackets remain
 };
 
 module.exports = { isValid };
-
 
